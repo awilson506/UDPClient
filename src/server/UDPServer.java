89655730f3b06405d3bdfe989538a226b1a3c32f
Adam Wilson <awilson@pubsvs.com>
2015-05-07 19:40:12 -0400

    updates

--- src/server/UDPServer.java
+++ src/server/UDPServer.java
@@ -1,72 +1,79 @@
 package server;
 
-import java.io.*; 
-import java.net.*; 
+import java.io.*;
+
+import client.UDPClient;
+
+import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 import threads.PrintPage;
-  
-public class UDPServer { 
-	
-  public static void main(String args[]) throws Exception 
-    { 
-     try
-     { 
-      DatagramSocket serverSocket = new DatagramSocket(21252); 
-      Integer packetsReceived = 0;
-      
-      
-  
-      while(true) 
-        { 
-  
-    	  byte[] receiveData = new byte[Math.min(1000, 1460)]; 
 
-          DatagramPacket receivePacket = 
-             new DatagramPacket(receiveData, receiveData.length); 
-          byte[] sendData  = new byte[Math.min(receiveData.length, 1460)]; 
-          System.out.println ("Waiting for datagram packet");
+public class UDPServer {
 
-          serverSocket.receive(receivePacket); 
-          //print the data from the server
-          
-          String sentence = new String(receivePacket.getData()); 
-  
-          InetAddress IPAddress = receivePacket.getAddress(); 
-          PrintPage p = new PrintPage(receivePacket.getData() , "output-server.txt");
-			p.start();
-			p.join();
-          int port = receivePacket.getPort(); 
-  
-          System.out.println ("From: " + IPAddress + ":" + port);
-          System.out.println ("Message: " + sentence);
+	public static void main(String args[]) throws Exception {
+		// System.out.println(args);
+		Integer packetsReceived = 0;
+		try {
+			DatagramSocket serverSocket = new DatagramSocket(21252);
 
-          String capitalizedSentence = sentence; 
+			int packetSize = 0;
 
-          sendData = capitalizedSentence.getBytes(); 
-          packetsReceived += sendData.length;
-          
-          sendData = convertToBytes(packetsReceived, ByteOrder.BIG_ENDIAN);
-          DatagramPacket sendPacket = 
-             new DatagramPacket(sendData, sendData.length, IPAddress, 
-                               port); 
-  
-          serverSocket.send(sendPacket); 
+			while (true) {
+				// System.out.println(packetSize);
+				byte[] receiveData = new byte[1460];
 
-        } 
+				DatagramPacket receivePacket = new DatagramPacket(receiveData,
+						receiveData.length);
 
-     }
-      catch (SocketException ex) {
-        System.out.println("UDP Port 21252 is occupied.");
-        System.exit(1);
-      }
+				System.out.println("Waiting for datagram packet");
 
-    } 
-  public static byte[] convertToBytes(int value, ByteOrder order)
-  {
-      ByteBuffer buffer = ByteBuffer.allocate(4); // in java, int takes 4 bytes.
-      buffer.order(order);
-      return buffer.putInt(value).array();
-  }
-}  
+				serverSocket.receive(receivePacket);
+				// print the data from the server
+
+				String sentence = new String(receivePacket.getData());
+				for (byte b : receivePacket.getData()) {
+					if (b != 0) {
+						packetSize++;
+					}
+				}
+				byte[] sendData = new byte[Math.min(packetSize, 1460)];
+
+				InetAddress IPAddress = receivePacket.getAddress();
+				PrintPage p = new PrintPage(receivePacket.getData(),
+						"output-server.txt");
+				p.start();
+				p.join();
+				int port = receivePacket.getPort();
+
+				System.out.println("From: " + IPAddress + ":" + port);
+				//System.out.println("Message: " + sentence);
+
+				String capitalizedSentence = sentence;
+
+				sendData = capitalizedSentence.getBytes();
+				packetsReceived += packetSize;
+
+				sendData = convertToBytes(packetsReceived, ByteOrder.BIG_ENDIAN);
+				DatagramPacket sendPacket = new DatagramPacket(sendData,
+						sendData.length, IPAddress, port);
+
+				serverSocket.send(sendPacket);
+
+			}
+
+		} catch (SocketException ex) {
+			System.out.println("UDP Port 21252 is occupied.");
+			System.exit(1);
+		}
+
+	}
+
+	public static byte[] convertToBytes(int value, ByteOrder order) {
+		ByteBuffer buffer = ByteBuffer.allocate(4); // in java, int takes 4
+													// bytes.
+		buffer.order(order);
+		return buffer.putInt(value).array();
+	}
+}
--- src/threads/SendPackets.java
+++ src/threads/SendPackets.java
@@ -9,73 +9,88 @@ import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-public class SendPackets extends Thread{
-	
+public class SendPackets extends Thread {
+
 	private byte[] sendData;
 	private byte[] receiveData;
 	private InetAddress IPAddress;
 	private int timeout;
-	
+	private static int ackCount;
 
-
-	public SendPackets(byte[] sendData, InetAddress IPAddress, byte[] receiveData, int timeout){
+	public SendPackets(byte[] sendData, InetAddress IPAddress,
+			byte[] receiveData, int timeout) {
 		this.sendData = sendData;
 		this.IPAddress = IPAddress;
 		this.receiveData = receiveData;
 		this.timeout = timeout;
 	}
 
-	
-	public void run(){
-		DatagramSocket clientSocket;
-		try {
-			
-			clientSocket = new DatagramSocket();
-			System.out.println("***********************");
-			System.out.println("Sending " + sendData.length
-					+ " bytes to server.");
-			DatagramPacket sendPacket = new DatagramPacket(sendData,
-					sendData.length, IPAddress, 21252);
-
-			clientSocket.send(sendPacket);
-
-			DatagramPacket receivePacket = new DatagramPacket(receiveData,
-					receiveData.length);
-
-			System.out.println("Waiting for return packet");
-			clientSocket.setSoTimeout(timeout);
-
+	public void run() {
+		for (int i = 0; i < 3; i++) {
+			DatagramSocket clientSocket;
 			try {
-				clientSocket.receive(receivePacket);
-				//String modifiedSentence = new String(receivePacket.getData());
-				int test = constructToInt(receivePacket.getData(),ByteOrder.BIG_ENDIAN);
-				InetAddress returnIPAddress = receivePacket.getAddress();
 
-				int port = receivePacket.getPort();
+				clientSocket = new DatagramSocket();
+				System.out.println("***********************");
+				System.out.println("Sending " + sendData.length
+						+ " bytes to server.");
+				DatagramPacket sendPacket = new DatagramPacket(sendData,
+						sendData.length, IPAddress, 21252);
 
-				System.out.println("From server at: " + returnIPAddress + ":"
-						+ port);
-				System.out.println("ACK: " + test);
-				clientSocket.close();
-				System.out.println("------------------------");
-			} catch (SocketTimeoutException ste) {
-				//try three times need to loop this thread and so something here if we're at count 3
-				System.out.println("Timeout Occurred: Packet assumed lost");
+				clientSocket.send(sendPacket);
+
+				DatagramPacket receivePacket = new DatagramPacket(receiveData,
+						receiveData.length);
+
+				System.out.println("Waiting for return packet");
+				clientSocket.setSoTimeout(timeout);
+
+				try {
+					clientSocket.receive(receivePacket);
+					// String modifiedSentence = new
+					// String(receivePacket.getData());
+					int test = constructToInt(receivePacket.getData(),
+							ByteOrder.BIG_ENDIAN);
+					InetAddress returnIPAddress = receivePacket.getAddress();
+
+					int port = receivePacket.getPort();
+
+					System.out.println("From server at: " + returnIPAddress
+							+ ":" + port);
+					System.out.println("ACK");
+					ackCount = test;
+					clientSocket.close();
+					i = 3;
+					System.out.println("------------------------");
+				} catch (SocketTimeoutException ste) {
+
+					// System.out.println("Attempt: " + i);
+					if (i == 2) {
+						System.out.println("FAIL");
+						System.exit(1);
+					}
+					// try three times need to loop this thread and so something
+					// here if we're at count 3
+					System.out.println("Timeout Occurred: Packet assumed lost");
+				}
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-	
-	
+
 	}
-	public static int constructToInt(byte[] byteArray, ByteOrder order)
-    {
-        ByteBuffer buffer = ByteBuffer.wrap(byteArray);
-        buffer.order(order);
-        return buffer.getInt();
-    }
+
+	public static int getACK() {
+		return ackCount;
+	}
+
+	public static int constructToInt(byte[] byteArray, ByteOrder order) {
+		ByteBuffer buffer = ByteBuffer.wrap(byteArray);
+		buffer.order(order);
+		return buffer.getInt();
+	}
 }
